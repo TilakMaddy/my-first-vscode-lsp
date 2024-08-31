af6209ddcd289f11d0aea6561bc88cd90fcd1bda
TilakMaddy <tilak.madichetti@gmail.com>
2024-08-31 23:36:21 +0530

    feat: basic jest test

---
+++ server/src/tests/server.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("lsp", () => {
+
+  test("can add 1 and 2", async () => {
+    const sum = 1 + 2;
+    expect(sum).toBe(3);
+
+  })
+
+});
